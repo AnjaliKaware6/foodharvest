0832da557c375339cde4ae714b8d8d29dcbdc964
Anjali Vijay Kaware <anjalikaware6402@gmail.com>
2024-01-03 14:36:14 +0530

    firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth';
-import 'firebase/compat/database'
+// import firebase from 'firebase/compat/app'
+// import 'firebase/compat/auth';
+// import 'firebase/compat/database'
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA2aItJnLX95AG9XDNh0oGnKiWmM1_3UNM",
-  authDomain: "food-harvest-1f40b.firebaseapp.com",
-  projectId: "food-harvest-1f40b",
-  storageBucket: "food-harvest-1f40b.appspot.com",
-  messagingSenderId: "161675900048",
-  appId: "1:161675900048:web:c3d8752f461973710f9415",
-  measurementId: "G-DTVSWETV4X"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA2aItJnLX95AG9XDNh0oGnKiWmM1_3UNM",
+//   authDomain: "food-harvest-1f40b.firebaseapp.com",
+//   projectId: "food-harvest-1f40b",
+//   storageBucket: "food-harvest-1f40b.appspot.com",
+//   messagingSenderId: "161675900048",
+//   appId: "1:161675900048:web:c3d8752f461973710f9415",
+//   measurementId: "G-DTVSWETV4X"
+// };
 
-const app = firebase.initializeApp(firebaseConfig);
-export default app 
+// const app = firebase.initializeApp(firebaseConfig);
+// export default app 
 
 
 
@@ -35,3 +35,23 @@ export default app
 // // Initialize Firebase
 // const app = firebase.initializeApp(firebaseConfig);
 // export default app 
+
+// Import the functions you need from the SDKs you need
+
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/database'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD__G90ZEG0DjM1GtRiNIlu6I73dfvMnWY",
+  authDomain: "food-harvest-3c4ad.firebaseapp.com",
+  projectId: "food-harvest-3c4ad",
+  storageBucket: "food-harvest-3c4ad.appspot.com",
+  messagingSenderId: "341928350239",
+  appId: "1:341928350239:web:d29ccd7d669a097894fb2a",
+  measurementId: "G-0MMERRR4E8"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+export default app 
